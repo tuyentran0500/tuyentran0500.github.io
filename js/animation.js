c2ee2a5eb4a039688242affefa8d54b78d5624bf
Tuyen Tran <tuyentran0500@gmail.com>
2020-07-28 22:05:30 +0900

    add background, music

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,10 @@
 function mode(image) {
+  // var element = document.getElementsByClassName("content");
+  // if (image == "sky") element.style.color = "black";
+  // else element.style.color = "blanchedalmond";
+  // alert(element.style.color);
   image = "img/" + image + ".jpg";
   image = "url('" + image + "')";
-  // alert(image);
   var element = document.getElementById("body");
   element.style.backgroundImage = image;
 }
@@ -20,6 +23,10 @@ function checkPass(){
     message.src = "img/jung so min.jpg";
     // message.style.width = "200px";
     // message.style.height = "300px";
+  }
+  else {
+    var message = document.getElementById("secret");
+    message.src = "";
   }
 }
 
